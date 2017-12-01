24a4046dc392d60b7a31a0ceac51669f6cfbc2ec
André Fernandes <greenstuga@gmail.com>
2017-12-01 19:00:15 +0000

    teste Professor

---
+++ GestorHorarios/test/ProfessorTest.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author André Fernandes
+ */
+public class ProfessorTest {
+    public static void main (String []argv)
+    {
+        ProfessorTest teste=new ProfessorTest();
+        teste.testGetNome();
+        teste.testSetNome();
+        teste.testGetIdade();
+        teste.testSetIdade();
+        teste.testGetGrau_Académico();
+        teste.testSetGrau_Académico();
+        
+    }
+    public ProfessorTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getNome method, of class Professor.
+     */
+    @Test
+    public void testGetNome() {
+        System.out.println("getNome");
+        String nome = "Lara";
+        Professor instance = new Professor(nome, 23, Grau_Académico.Douturamento);
+        System.out.println(instance.getNome());    
+    }
+
+    /**
+     * Test of setNome method, of class Professor.
+     */
+    @Test
+    public void testSetNome() {
+         System.out.println("setNome");
+        String nome = "Joaquim";
+        Professor instance = new Professor("Lara", 23, Grau_Académico.Douturamento);
+        instance.setNome(nome);
+        System.out.println(instance.getNome());  
+    }
+
+    /**
+     * Test of getIdade method, of class Professor.
+     */
+    @Test
+    public void testGetIdade() {
+        System.out.println("getIdade");
+        int numero = 13;
+        Professor instance = new Professor("Lara", numero, Grau_Académico.Douturamento);
+        System.out.println(instance.getIdade());  
+    }
+
+    /**
+     * Test of setIdade method, of class Professor.
+     */
+    @Test
+    public void testSetIdade() {
+        System.out.println("setIdade");
+        int idade = 13;
+        Professor instance = new Professor("Lara", 24, Grau_Académico.Douturamento);
+        instance.setIdade(idade);
+        System.out.println(instance.getIdade()); 
+    }
+
+    /**
+     * Test of getGrau_Académico method, of class Professor.
+     */
+    @Test
+    public void testGetGrau_Académico() {
+        System.out.println("getGrau_Academico");
+        Grau_Académico douturamento=Grau_Académico.Licenciamento;
+        Professor instance = new Professor("Lara", 24, douturamento);
+        System.out.println(instance.getGrau_Académico()); 
+    }
+
+    /**
+     * Test of setGrau_Académico method, of class Professor.
+     */
+    @Test
+    public void testSetGrau_Académico() {
+        System.out.println("setGrau_Academico");
+        Grau_Académico grau_Académico = Grau_Académico.Licenciamento;
+        Professor instance = new Professor("Lara", 24, Grau_Académico.Douturamento);
+        instance.setGrau_Académico(grau_Académico);  
+        System.out.println(instance.getGrau_Académico()); 
+    }
+    
+}
