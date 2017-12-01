8334b939e3af7aa6595303dd8147d17d2be77c4f
André Fernandes <greenstuga@gmail.com>
2017-12-01 15:36:45 +0000

    Criacao da Turma

---
+++ GestorHorarios/src/Turma.java
@@ -0,0 +1,57 @@
+
+enum Tipo_Turma{
+    Prática,
+    Teórica,
+    Teórica_ou_Prática;
+}
+
+public class Turma {
+    
+    
+    private int numero;
+    private String disciplina;
+    private Tipo_Turma tipoTurma;
+
+    public Turma(int numero,String disciplina, Tipo_Turma tipoTurma)throws IllegalArgumentException
+    {
+        if(numero>0){
+            this.numero=numero;
+        }else{
+            throw new IllegalArgumentException("Formatação errada no campo Número");
+        }
+        
+        if(disciplina==null){
+            this.disciplina=disciplina;
+        }else{
+            throw new IllegalArgumentException("Formatação errada no campo Disciplina");
+        }
+        
+        this.tipoTurma=tipoTurma;
+    }
+    
+    public Tipo_Turma getTipoTurma() {
+        return tipoTurma;
+    }
+
+    public void setTipoTurma(Tipo_Turma tipoTurma) {
+        this.tipoTurma = tipoTurma;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getDisciplina() {
+        return disciplina;
+    }
+
+    public void setDisciplina(String disciplina) {
+        this.disciplina = disciplina;
+    }
+    
+    
+}
